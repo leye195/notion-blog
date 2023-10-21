b2461ea412a9a9f70c7a0fe1e116a802f63a0502
leye195 <leye195@naver.com>
2023-10-22 00:05:33 +0900

    feat: add Github, RSS, Email Icon

--- src/components/icon/index.tsx
+++ src/components/icon/index.tsx
@@ -33,3 +33,65 @@ export const Dark = () => (
     />
   </svg>
 );
+
+export const GitHub = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    stroke="currentColor"
+    viewBox="0 0 24 24"
+    className="humbleicons hi-brand-github"
+  >
+    <path
+      xmlns="http://www.w3.org/2000/svg"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      d="M8.5 21c2-2-.5-6 3.5-6m0 0c-3 0-7-1-7-5 0-1.445.116-2.89.963-4V3L9 4.283C9.821 4.101 10.81 4 12 4s2.178.1 3 .283L18 3v2.952c.88 1.116 1 2.582 1 4.048 0 4-4 5-7 5zm0 0c4 0 1.5 4 3.5 6M3 15c3 0 1.5 4 6 3"
+    />
+  </svg>
+);
+
+export const Email = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    stroke="currentColor"
+    viewBox="0 0 24 24"
+    className="humbleicons hi-mail"
+  >
+    <g xmlns="http://www.w3.org/2000/svg" stroke="currentColor" strokeWidth="2">
+      <path
+        strokeLinejoin="round"
+        d="M20 6H4a1 1 0 00-1 1v10a1 1 0 001 1h16a1 1 0 001-1V7a1 1 0 00-1-1z"
+      />
+      <path strokeLinecap="round" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8" />
+    </g>
+  </svg>
+);
+
+export const RSS = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    stroke="currentColor"
+    viewBox="0 0 24 24"
+    className="humbleicons hi-rss"
+  >
+    <path
+      xmlns="http://www.w3.org/2000/svg"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeWidth="2"
+      d="M13 19a8 8 0 00-8-8M19 19c0-7.732-6.268-14-14-14"
+    />
+    <circle
+      xmlns="http://www.w3.org/2000/svg"
+      cx="6"
+      cy="18"
+      r="2"
+      fill="currentColor"
+    />
+  </svg>
+);
