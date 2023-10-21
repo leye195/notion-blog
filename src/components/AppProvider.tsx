5aa8373ddb7cf9146bb83592bf0f1843e79f5cf8
leye195 <leye195@naver.com>
2023-10-22 00:04:37 +0900

    feat: apply react-query

--- src/components/AppProvider.tsx
+++ src/components/AppProvider.tsx
@@ -1,15 +1,20 @@
 "use client";
 
 import { ComponentProps } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import StyledComponentsRegistry from "@/libs/registry";
 import GlobalStyle from "@/styles/global";
 
+const queryClient = new QueryClient();
+
 const AppProvider = ({ children }: ComponentProps<"div">) => {
   return (
-    <StyledComponentsRegistry>
-      <GlobalStyle />
-      {children}
-    </StyledComponentsRegistry>
+    <QueryClientProvider client={queryClient}>
+      <StyledComponentsRegistry>
+        <GlobalStyle />
+        {children}
+      </StyledComponentsRegistry>
+    </QueryClientProvider>
   );
 };
 
