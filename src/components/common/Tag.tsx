fa091793d1a4d6d42f95f164f60b17558d40d994
leye195 <leye195@naver.com>
2023-10-22 20:15:08 +0900

    feat: update Tag to apply outline option

--- src/components/common/Tag.tsx
+++ src/components/common/Tag.tsx
@@ -1,10 +1,33 @@
+import { classnames } from "@/libs/utils";
+
 type Props = {
+  type?: "normal" | "outline";
+  size?: "sm" | "md" | "lg";
+  color?: string;
   name: string;
 };
 
-const Tag = ({ name }: Props) => {
+const Tag = ({
+  name,
+  type = "normal",
+  color = "white",
+  size = "sm",
+}: Props) => {
   return (
-    <div className="px-[0.5rem] rounded-md  text-sm bg-blue-200">{name}</div>
+    <div
+      className={classnames(
+        "px-[0.5rem]",
+        "rounded-md",
+        `text-[${color}]`,
+        `text-${size}`,
+        type === "normal" ? "bg-blue-200" : "bg-white",
+        type === "normal"
+          ? "border-transparent"
+          : "hover:bg-blue-200 border-2 border-blue-200 transition-all"
+      )}
+    >
+      {name}
+    </div>
   );
 };
 
