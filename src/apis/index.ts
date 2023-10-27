f3c44ed189cda7b53c47db3d39d4fdd5b5b9e6a0
leye195 <leye195@naver.com>
2023-10-28 00:57:37 +0900

    feat: add getPost api function

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -8,6 +8,12 @@ const api = axios.create({
 
 export const getTags = () => api.get("/api/tags");
 
+export const getPost = async (id: string) => {
+  const res = await fetch(`${process.env.BASE_URL}/api/post?id=${id}`);
+  const data = await res.json();
+  return data;
+};
+
 export const getPosts = async (category: string, count?: number) => {
   const res = await fetch(
     `${process.env.BASE_URL}/api/posts?category=${category}${
