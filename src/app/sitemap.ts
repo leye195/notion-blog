1d6bf700275b861b7c54265e07fcee4fd4759af0
leye195 <leye195@naver.com>
2023-10-29 17:20:47 +0900

    feat: add post list on sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,10 +1,28 @@
+import { getPosts } from "@/apis";
+import type { notionDBRowStructed as Post } from "@/types/notion";
 export default async function sitemap() {
-  const baseUrl = "http://localhost:3000";
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+  const posts = await getPosts("all");
+  const postSitemaps = posts.map(async (post: Post) => {
+    return {
+      url: `${baseUrl}/posts/${post.id}`,
+      lastModified: new Date(),
+    };
+  });
 
   return [
     {
       url: baseUrl,
       lastModified: new Date(),
     },
+    {
+      url: `${baseUrl}/posts`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/resume`,
+      lastModified: new Date(),
+    },
+    ...postSitemaps,
   ];
 }
